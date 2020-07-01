48e72203500d5e85717cc1f5f20d78cb5a8cf950
TXT-02 <txt-02@macbook-pro-5.attlocal.net>
2020-06-30 20:54:10 -0700

    page wont load

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { MbscModule } from '@mobiscroll/angular';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
+import {Chart}  from 'chart.js';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
@@ -16,7 +16,7 @@ import { AppComponent } from './app.component';
   entryComponents: [],
   imports: [ 
     FormsModule,  
-    MbscModule, BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+    MbscModule, BrowserModule, IonicModule.forRoot(), AppRoutingModule, Chart,],
   providers: [
     StatusBar,
     SplashScreen,
