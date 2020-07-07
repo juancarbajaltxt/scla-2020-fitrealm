6799506f85a027f53fd0439feb4c7d33155a3f97
Mega-57 <l.cnor752@gmail.com>
2020-07-07 11:37:16 -0700

    overview return button is the final hurdle

--- src/app/overview/overview-routing.module.ts
+++ src/app/overview/overview-routing.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
 import { OverviewPage } from './overview.page';
-
+import { Tab1Page } from '../tab1/tab1.page';
 
 const routes: Routes = [
   {
     path: '',
     component: OverviewPage
+  },
+  {
+    path: '/tabs/tab1',
+    component: Tab1Page
   },
 ];
 
--- src/app/overview/overview.module.ts
+++ src/app/overview/overview.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
+//import { DataService } from '../services/data.service';
 import { IonicModule } from '@ionic/angular';
 
 import { OverviewPageRoutingModule } from './overview-routing.module';
@@ -13,7 +13,8 @@ import { OverviewPage } from './overview.page';
     CommonModule,
     FormsModule,
     IonicModule,
-    OverviewPageRoutingModule
+    OverviewPageRoutingModule,
+    //DataService,
   ],
   declarations: [OverviewPage]
 })
