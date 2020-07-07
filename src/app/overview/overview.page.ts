80c75696f80ba270859a2b40446124a35b6d9b1f
Mega-57 <l.cnor752@gmail.com>
2020-07-07 11:20:10 -0700

    __

--- src/app/overview/overview.page.ts
+++ src/app/overview/overview.page.ts
@@ -12,17 +12,20 @@ export class OverviewPage implements OnInit {
 
 data: any;
 
-  constructor(private dataService: DataService) { }
+  constructor(private dataService: DataService) { 
 
-  
+    
 
+  }
 
   ngOnInit() {
 
     var tValues = this.dataService.getData();
     console.log(tValues.calLost, tValues.pushupsDone, tValues.ptimesDone);
-    var tvalViewable = {calsLost: tValues.calLost, pDone: tValues.pushupsDone, ptDone: tValues.ptimesDone};
-    console.log(tvalViewable.calsLost);
+
+    this.data = {cLost: tValues.calLost}
+    console.log(this.data.cLost);
+
   }
 
 }
